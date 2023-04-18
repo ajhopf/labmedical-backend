29221bd05418ec6fce4e3a5bafc26e358b21e3fb
André Hopf <andre.hopf@gmail.com>
2023-04-18 12:34:31 -0300

    Implementa atualizarExame

--- src/main/java/br/com/labmedical/backend/controllers/ExameController.java
+++ src/main/java/br/com/labmedical/backend/controllers/ExameController.java
@@ -1,15 +1,13 @@
 package br.com.labmedical.backend.controllers;
 
 import br.com.labmedical.backend.dtos.exame.ExamePostRequestDto;
+import br.com.labmedical.backend.dtos.exame.ExamePutRequestDto;
 import br.com.labmedical.backend.dtos.exame.ExameResponseDto;
 import br.com.labmedical.backend.services.ExameService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
@@ -32,5 +30,15 @@ public class ExameController {
                 .toUri();
 
         return ResponseEntity.created(uri).body(exame);
+    }
+
+    @PutMapping("/{identificador}")
+    public ResponseEntity<ExameResponseDto> atualizarExame(
+            @PathVariable Long identificador,
+            @RequestBody ExamePutRequestDto requestDto
+    ) {
+        ExameResponseDto exame = service.atualizarExame(identificador, requestDto);
+
+        return ResponseEntity.ok(exame);
     }
 }
--- src/main/java/br/com/labmedical/backend/services/ExameService.java
+++ src/main/java/br/com/labmedical/backend/services/ExameService.java
@@ -1,12 +1,14 @@
 package br.com.labmedical.backend.services;
 
 import br.com.labmedical.backend.dtos.exame.ExamePostRequestDto;
+import br.com.labmedical.backend.dtos.exame.ExamePutRequestDto;
 import br.com.labmedical.backend.dtos.exame.ExameResponseDto;
 import br.com.labmedical.backend.mappers.ExameMapper;
 import br.com.labmedical.backend.models.Exame;
 import br.com.labmedical.backend.models.Paciente;
 import br.com.labmedical.backend.repositories.ExameRepository;
 import br.com.labmedical.backend.repositories.PacienteRepository;
+import br.com.labmedical.backend.services.helpers.CadastroHelper;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -32,6 +34,37 @@ public class ExameService {
         String dataEHora = LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss"));
 
         exame.setHoraEData(dataEHora);
+        exame = repository.save(exame);
+
+        return mapper.map(exame);
+    }
+
+    public ExameResponseDto atualizarExame(Long identificador, ExamePutRequestDto requestDto) {
+        Exame exame = repository.findById(identificador)
+                .orElseThrow(() -> new EntityNotFoundException("Não foi possível encontra um exame com o id " + identificador + ". Atualização não realizada."));
+
+        if (CadastroHelper.contemInformacao(requestDto.getNomeDoExame())) {
+            exame.setNomeDoExame(requestDto.getNomeDoExame());
+        }
+        if (CadastroHelper.contemInformacao(requestDto.getTipoDoExame())) {
+            exame.setTipoDoExame(requestDto.getTipoDoExame());
+        }
+        if (CadastroHelper.contemInformacao(requestDto.getLaboratorio())) {
+            exame.setLaboratorio(requestDto.getLaboratorio());
+        }
+        if (CadastroHelper.contemInformacao(requestDto.getArquivoDeExame())) {
+            exame.setArquivoDeExame(requestDto.getArquivoDeExame());
+        }
+        if (CadastroHelper.contemInformacao(requestDto.getResultadoDoExame())) {
+            exame.setResultadoDoExame(requestDto.getResultadoDoExame());
+        }
+        if (requestDto.getPacienteId() != null) {
+            Paciente paciente = pacienteRepository.findById(requestDto.getPacienteId())
+                    .orElseThrow(() -> new EntityNotFoundException("Atualização de exame não realizada. Não foi possível encontrar um paciente com o id " + requestDto.getPacienteId() + "."));
+
+            exame.setPaciente(paciente);
+        }
+
         exame = repository.save(exame);
 
         return mapper.map(exame);
